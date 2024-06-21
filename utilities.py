accaa85a6d15df6c484ede8c9a70001ad12dd5a4
PC_FRANCESCO\frenci <frencimanto@gmail.com>
2024-06-21 16:38:10 +0200

    added precision-recall-accuracy metrics

--- utilities.py
+++ utilities.py
@@ -5,6 +5,9 @@ from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.vgg16 import preprocess_input
 from tensorflow.keras.applications import VGG16
 from tensorflow.keras.models import Model
+from collections import namedtuple
+from sklearn.decomposition import PCA
+import os
 
 #device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
@@ -78,3 +81,61 @@ def VGG16_features(img_path, model, layer_name,visualize):
 
     return intermediate_output.flatten()
 
+
+
+def path_discovery(img_dir):
+    """
+    Discover image paths and corresponding labels in a directory.
+
+    Parameters:
+    img_dir (str): The root directory.
+
+    Returns res(img_paths, dirs_visited, labels):
+        - img_paths (list): List of full image file paths.
+        - dirs_visited (list): List of directories visited.
+        - labels (np.ndarray): Array of integer labels corresponding to each image's directory (from 1 to n).
+    """
+    discovery_results= namedtuple('discovery_results',['img_paths', 'dirs_visited', 'labels'])
+    
+    labels= np.array([],dtype=int)
+    img_paths=[]
+    dirs_visited=[]
+    k=0
+    for root, dirs, files in os.walk(img_dir):
+        dirs_visited.append(root)
+        for filename in files:
+            img_path= os.path.join(root,filename)
+            print(img_path)
+            img_paths.append(img_path) 
+            labels=np.append(labels,k)
+        k=k+1
+    
+    return discovery_results(img_paths, dirs_visited, labels)
+
+
+#pca component extraction mantaining 90% of test variance
+def pca_extraction(feature_list, variance_percentage):
+    """
+    extract PCA component according to specified variance percentage
+
+    Parameters:
+    - feature_list
+    - variance_percentage
+
+    Returns results(pca,pca_result):
+        - pca (class object)
+        - pca_result (list) list of pca eigenvectors
+    """
+    results= namedtuple('results',['pca', 'pca_result'])
+    
+    if(variance_percentage<0 or variance_percentage>1):
+        raise ValueError("variance_percentage must be between 0 and 1")
+    pca= PCA()
+    pca.fit(feature_list)
+    cumulative_explained_variance = np.cumsum(pca.explained_variance_ratio_)
+    pca_components=np.argmax(cumulative_explained_variance>variance_percentage)+1
+    print(f"number of PCA components explaining {variance_percentage * 100}% of variance: {pca_components}")
+    #applying pca with selected components number
+    pca = PCA(n_components=pca_components)
+    pca_result = pca.fit_transform(feature_list)
+    return results(pca,pca_result)
